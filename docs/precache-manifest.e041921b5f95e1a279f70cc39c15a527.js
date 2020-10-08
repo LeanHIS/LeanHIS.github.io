adbcf09747ccb9c88a8c17970e6b1a8e040f5df8
Jan Mewes <jan.mewes@experimental-software.com>
2020-10-08 12:28:13 +0200

    Add hello world page

---
+++ docs/precache-manifest.e041921b5f95e1a279f70cc39c15a527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75aee31c54f9c56dd3465f816763787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "049644e51f4ba9ec302a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d7ef76ee20120b5d165a",
+    "url": "/static/js/2.62ba8dbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62ba8dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049644e51f4ba9ec302a",
+    "url": "/static/js/main.dc4bb2fe.chunk.js"
+  },
+  {
+    "revision": "b0a0074f679d8690e284",
+    "url": "/static/js/runtime-main.7ea1fa1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
